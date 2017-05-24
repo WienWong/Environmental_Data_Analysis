b6afc8cf3fee662fa586876d8094eb9eed2b3a35
Weihua Wang <wangzichengfu@gmail.com>
2017-05-24 15:05:27 +0200

    Create dirpath.R
    Return the directory path.

---
+++ dirpath.R
@@ -0,0 +1,24 @@
+dirpath <- function(month,day,SW) {
+
+  # Om Mani Padme Hum !
+  # This function will generate a directory path for the concentration measurement.
+  # 2017-03-14, 1st built, 2017-05-01, 2nd modified, by Wang Weihua
+  # SW stands for 'switch', thus the concentration folder ("conc") or GPS folder ("gps") can be selected.
+  
+  if (SW=="conc") {
+    if (1<=day & day<=9){
+      tmpath <- paste('/PATH_TO_THE_FOLDER/170', toString(month), '0', toString(day), '/', sep="")
+    } else if (10<=day & day<=31){
+      tmpath <- paste('/home/wien/PATH_TO_THE_FOLDER/170', toString(month), toString(day), '/', sep="")
+    }
+    return(tmpath)
+  } else if (SW=="gps") {
+    if (1<=day & day<=9){
+      tmpath <- paste( "/PATH_TO_THE_FOLDER/170", toString(month), '0', toString(day), "/", sep="")
+    } else if (10<=day & day<=31){
+      tmpath <- paste( "/home/wien/PATH_TO_THE_FOLDER/170", toString(month), toString(day), "/", sep="")
+    }
+    return(tmpath) 
+  }
+  
+}
